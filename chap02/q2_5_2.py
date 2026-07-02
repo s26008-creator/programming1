74d45e7ed89fa813beb107199afdba3dbcbb4433
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-02 03:34:48 +0000

    2.5文字列

---
+++ chap02/q2_5_2.py
@@ -0,0 +1,20 @@
+#answer = '''❍ ● ● ●
+#● ❍ ● ●
+#● ● ❍ ●
+#● ● ● ❍
+
+w = '❍'
+b = '●'
+answer = w + b*3 + '\n' + b + w + b*2 +'\n' + b*2 + w + b +'\n' + b*3 + w
+print(answer)
+
+answer = ''
+for i in range(4):
+    for j in range(4):
+        if i == j:
+            answer += w
+        else:
+            answer += b
+    answer += '\n'
+    print(answer)
+
