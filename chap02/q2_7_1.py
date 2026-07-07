6ed9f52915d96fa6e3b5f2b908593b4228535a84
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-07 01:26:53 +0000

    2.7リスト

---
+++ chap02/q2_7_1.py
@@ -0,0 +1,6 @@
+my_list = []
+my_list.append('orange')
+my_list.append('apple')
+my_list.append('grape')
+my_list.append('banana')
+print(my_list)
---
+++ chap02/q2_7_2.py
@@ -0,0 +1,8 @@
+my_list = ['orange','apple','grape','banana']
+my_list.sort()
+print(my_list)
+
+my_list2 = ['orange','apple','grape','banana']                                           
+my_list2.sort(reverse = True)                                                                          
+print(my_list2)
+
