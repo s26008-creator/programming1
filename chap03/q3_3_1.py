f7b0dd25d6bd1dc70d2fc07f1bb61dbfb37fc9a4
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-08 03:13:09 +0000

    3.3 for文の基礎

---
+++ chap03/q3_3_1.py
@@ -0,0 +1,7 @@
+my_list = ['tokyo','osaka','fukuoka','aichi','kyoto','chiba','saitama','gunma']
+my_list_2 = []
+for s in my_list:
+    if len(s) >= 6:
+        my_list_2.append(s)
+print(my_list_2)
+
---
+++ chap03/q3_3_2.py
@@ -0,0 +1,6 @@
+my_list = [1,1,2,3,5,8,13]
+x = 0
+for i in my_list:
+    if i % 2 != 0:
+            x += i
+print(x)
