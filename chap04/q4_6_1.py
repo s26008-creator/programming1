9946dadeafa6d14f946011d903a8f236e1eca318
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-16 04:56:49 +0000

    4-6 ランダム式

---
+++ chap04/q4_6_1.py
@@ -0,0 +1,3 @@
+str_num_list = map(lambda x: "{:04}" .format(x), range(1,8))
+str_num_list
+print(list(str_num_list))
---
+++ chap04/q4_6_2.py
@@ -0,0 +1,22 @@
+import random
+def generate_students_data(num_student=10):
+
+    student_data = []
+    for i in range(num_students):
+        name = 'n' + str(rondom.randint(10,50))
+        height = random.randint(150,190)
+        weight = random.randint(50,80)
+        students_data.append((name,height,weight))
+        if i == 0: print('i, name, height, weight')
+        if i < 2 or i == num_students -1:
+            print(i,name,height,weight)
+        elif i == 2:
+            print('...')
+        return students_data
+students_data = generate_students_data(10)
+
+studentd_data
+
+students_by_height = sorted(students_data, kye=lambda s: s[1])
+students_by_weight = sorted(students_data, kye=lambda s: s[2])
+
