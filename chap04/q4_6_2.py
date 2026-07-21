6c13b43c2e86341b849179edcc222c44969b11e6
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-21 01:20:45 +0000

    4-6 ランダム式

--- chap04/q4_6_2.py
+++ chap04/q4_6_2.py
@@ -20,3 +20,10 @@ students_data
 students_by_height = sorted(students_data, key=lambda s: s[1])
 students_by_weight = sorted(students_data, key=lambda s: s[2])
 
+print('\nSort by height')
+for student in students_by_height:
+    print(student)
+
+print('\nSort by weight')
+for student in students_by_weight:
+    print(student)
