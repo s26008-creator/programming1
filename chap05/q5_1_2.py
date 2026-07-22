204920e7a868e0c6335369ed2f7a89c5b091e937
s26008-creator <s26008@std.it-college.ac.jp>
2026-07-22 00:37:28 +0000

    5.1 リストのメソッド

---
+++ chap05/q5_1_2.py
@@ -0,0 +1,8 @@
+stack = []
+stack.append(1)
+print('stack:', stack)
+stack.append(2)
+print('stack:', stack)
+print('pop 1st value:', stack.pop())
+print('pop 2st value:', stack.pop())
+print('stack:', stack)
---
+++ chap05/q5_1_3.py
@@ -0,0 +1,8 @@
+queue = []
+queue.append(1)
+print('queue:',queue)
+queue.append(2)
+print('pueue:',queue)
+print('pop form queue 1st value:', queue.pop(0))
+print('pop form queue 2st value:', queue.pop(0))
+print('queue:', queue)
