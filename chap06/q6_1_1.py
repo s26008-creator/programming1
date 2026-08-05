a99a1a02f3cd0d9f51aace82019285b09a5f57eb
s26008-creator <s26008@std.it-college.ac.jp>
2026-08-05 01:59:43 +0000

    6.1 クラスの基本

---
+++ chap06/q6_1_1.py
@@ -0,0 +1,9 @@
+class Cat:
+    cry = "ニャー"
+    legs = 4
+    is_animal = True
+
+tama = Cat()
+print("鳴き声: {}, 足の数: {}, 動物: {}".format(tama.cry, tama.legs, tama.is_animal))
+
+
---
+++ chap06/q6_1_2.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self,
+                name = '',
+                nationality = '',
+                birth = '',
+                address = ''):
+        self.name = name
+        self.nationality
+        self.birth
+        self.address
+
+def show_attributes(self):
+    print("名前:", self.name)
+    print("国籍:", self.nationality)
+    print("生まれた年:", self.birth)
+    print("住んでいるところ:", self.address)
+    
+namae = Person('大貴','日本','2006','沖縄県豊見城市')
+name.show_attributes()
