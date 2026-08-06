0f56906c548624f3e6b0da7a3a27862b0bb50903
s26008-creator <s26008@std.it-college.ac.jp>
2026-08-06 02:39:42 +0000

    6.1 クラスの基本

--- chap06/q6_1_2.py
+++ chap06/q6_1_2.py
@@ -5,15 +5,15 @@ class Person:
                 birth = '',
                 address = ''):
         self.name = name
-        self.nationality
-        self.birth
-        self.address
+        self.nationality = nationality
+        self.birth = birth
+        self.address = address
 
-def show_attributes(self):
-    print("名前:", self.name)
-    print("国籍:", self.nationality)
-    print("生まれた年:", self.birth)
-    print("住んでいるところ:", self.address)
+    def show_attributes(self):
+        print("名前:", self.name)
+        print("国籍:", self.nationality)
+        print("生まれた年:", self.birth)
+        print("住んでいるところ:", self.address)
     
-namae = Person('大貴','日本','2006','沖縄県豊見城市')
+name = Person('大貴','日本','2006','沖縄県豊見城市')
 name.show_attributes()
